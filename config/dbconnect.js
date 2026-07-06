c3c7ed66740831e255cdccdfa935853028633240
souravp842 <dev4.souravpuri@gmail.com>
2026-07-06 19:19:10 +0530

    remove database url

--- config/dbconnect.js
+++ config/dbconnect.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const dbconnect = async () => {
 try{
-   await mongoose.connect(`mongodb+srv://appskaswebtech_db_user:Abinash7077@cluster0.ius7lms.mongodb.net/finallearn`);
+   await mongoose.connect(process.env.DATABASEURL);
    console.log('Database connected successfully');
 }catch(err){
     console.log(err);
