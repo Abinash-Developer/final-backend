dcb793567c66f438a1d4eefd949f12eadee1b106
souravp842 <dev4.souravpuri@gmail.com>
2026-07-07 19:05:38 +0530

    remove not used URL

--- app.js
+++ app.js
@@ -9,11 +9,8 @@ app.use(express.json());
 app.use("/api/user", require("./routes/user"));
 
 app.use((err, req, res, next) => {
-  console.error(err.stack); 
-
   const statusCode = err.statusCode || 500;
   const message = err.message || "Internal Server Error";
-
   res.status(statusCode).json({
     success: false,
     status: statusCode,
